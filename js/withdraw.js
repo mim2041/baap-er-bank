6c66fc6a410d888346025c9aeeb806ba54869731
mim2041 <suraiyamim338@gmail.com>
2023-02-14 23:11:42 +0600

    baap er bank website done

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,4 +1,5 @@
 document.getElementById('btn-Withdraw').addEventListener('click',function(){
+    console.log('clicked')
     const WithdrawField = document.getElementById('withdraw-field');
     const withdrawAmount = parseFloat(WithdrawField.value);
 
@@ -6,8 +7,13 @@ document.getElementById('btn-Withdraw').addEventListener('click',function(){
     const previousWithdrawBalance = parseFloat(withdrawTotal.innerText);
 
     const newWithdrawTotal = withdrawAmount + previousWithdrawBalance;
-    console.log(newWithdrawTotal);
-    WithdrawField.innerText = newWithdrawTotal;
+    withdrawTotal.innerText = newWithdrawTotal;
 
     WithdrawField.value = '';
+
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalance = parseInt(balanceTotal.innerText);
+
+    const newBalanceTotal = previousBalance - withdrawAmount;
+    balanceTotal.innerText = newBalanceTotal;
 })
